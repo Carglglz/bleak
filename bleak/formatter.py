982d1be06155ae0c0c75e54254e00e1eaf61490e
Carlosgg <carlosgilglez@gmail.com>
2020-08-10 16:29:33 +0100

    typo and assert fix

--- bleak/formatter.py
+++ bleak/formatter.py
@@ -79,7 +79,7 @@ def encode_SFLOAT_ieee11073(value, precision=1, debug=False):
     The SFLOAT-Type is defined as a 16-bit value with 12-bit mantissa and 4-bit exponent
     """
     val = int(value * (10 ** precision))
-    assert val < 2**11, 'Mantissa to big'
+    assert val < 2**11, 'Mantissa too big'
     encoded = ((precision << 12) + twos_comp(val, 12)
                ).to_bytes(2, 'little', signed=True)
     if debug:
